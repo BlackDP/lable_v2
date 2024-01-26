ed5bebb06b618396167a6eb89142ec07a8dbeb6e
BlackDP <blackdp@ya.ru>
2024-01-26 16:53:03 +0300

    Добавлена модель профиля пользователя

--- core/userprofile/models.py
+++ core/userprofile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    name = models.OneToOneField(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    
+
+    class Meta:
+        verbose_name = 'Профиль пользователя'
+        verbose_name_plural = 'Профили пользователей'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('user_profile_detail', kwargs={'pk': self.pk})
