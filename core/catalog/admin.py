7159ba283ce345262e4d78d7c879363619b60580
BlackDP <blackdp@ya.ru>
2024-01-30 18:22:13 +0300

    Product и Category добавлены в админку

--- core/catalog/admin.py
+++ core/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 # Register your models here.
+from .models import Product, Category
+
+
+admin.site.register(Product)
+admin.site.register(Category, MPTTModelAdmin)
