0d473d745e0e9d10b8ae5dc0c4f85bd315868366
BlackDP <blackdp@ya.ru>
2024-01-30 18:04:49 +0300

    Добавлена модель товаров

--- core/catalog/models.py
+++ core/catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+
+class Product(models.Model):
+    name = models.CharField('Наименование', max_length=250)
+    
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', kwargs={'pk': self.pk})
+
+
