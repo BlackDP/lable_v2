d3a77b643d33bb5113708f9ea1033fd4c45ee092
BlackDP <blackdp@ya.ru>
2024-02-01 13:55:55 +0300

    Добавлены маршруты к главной странице

--- core/core/urls.py
+++ core/core/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('storefront.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', include('userprofile.urls')),
